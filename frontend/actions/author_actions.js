a9569c9d7fc09d255b6aae732dceb4e45956f29f
Ariton Sefedini <aritonsefedini@icloud.com>
2021-05-03 10:51:36 -0400

    created authors and books models and controllers

--- frontend/actions/author_actions.js
+++ frontend/actions/author_actions.js
@@ -1,4 +1,27 @@
 import * as AuthorApiUtil from '../util/author_api_util';
 
 export const RECEIVE_ALL_AUTHORS = "RECEIVE_ALL_AUTHORS";
-export const RECEVIE_CURRENT_AUTHOR = 
+export const RECEIVE_CURRENT_AUTHOR = "RECEIVE_CURRENT_AUTHOR";
+export const REMOVE_CURRENT_AUTHOR = "REMOVE_CURRENT_AUTHOR";
+
+
+const receiveAuthors = authors => {
+    return {
+        type: RECEIVE_ALL_AUTHORS,
+        authors
+    }
+}
+
+const receiveAuthor = author => {
+    return {
+        type: RECEIVE_CURRENT_AUTHOR,
+        author
+    }
+}
+
+const removeAuthor = authorId => {
+    return {
+        type: REMOVE_CURRENT_AUTHOR,
+        authorId
+    }
+}
