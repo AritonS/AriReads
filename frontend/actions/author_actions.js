4087ec423683e04a2f0efb08bfad7af151deadb7
Ariton Sefedini <aritonsefedini@icloud.com>
2021-05-04 10:52:55 -0400

    arg

--- frontend/actions/author_actions.js
+++ frontend/actions/author_actions.js
@@ -5,6 +5,7 @@ export const RECEIVE_CURRENT_AUTHOR = "RECEIVE_CURRENT_AUTHOR";
 export const REMOVE_CURRENT_AUTHOR = "REMOVE_CURRENT_AUTHOR";
 
 
+
 const receiveAuthors = authors => {
     return {
         type: RECEIVE_ALL_AUTHORS,
@@ -24,4 +25,29 @@ const removeAuthor = authorId => {
         type: REMOVE_CURRENT_AUTHOR,
         authorId
     }
+}
+
+export const fetchAuthors = () => dispatch => {
+    return AuthorApiUtil.fetchAuthors()
+        .then(authors => dispatch(receiveAuthors(authors)))
+}
+
+export const fetchAuthor = authorId => dispatch => {
+    return AuthorApiUtil.fetchAuthor(authorId)
+        .then(author => dispatch(receiveAuthor(author)))
+}
+
+export const createAuthor = author => dispatch => {
+    return AuthorApiUtil.updateAuthor(author)
+        .then(createdAuthor => dispatch(receiveAuthor(author)))
+}
+
+export const updateAuthor = author => dispatch => {
+    return AuthorApiUtil.updateAuthor(author)
+        .then(updatedAuthor => dispatch(receiveAuthor(author)))
+}
+
+export const deleteAuthor = authorId => dispatch => {
+    return AuthorApiUtil.deleteAuthor(authorId)
+        .then(() => dispatch(removeAuthor(authorId)))
 }
